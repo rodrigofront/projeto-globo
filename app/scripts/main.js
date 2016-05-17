7d2ada9c28142d0e7a6b2a8b05d3a5ea19079929
Rodrigo Nogueira <fale.rodrigo@gmail.com>
2016-05-17 19:09:48 -0300

    inserindo orientacao a objeto

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,96 +1,92 @@
 $(function(){
-	
-	//renderizar repositorios do globocom
-	var org   	   = 'https://api.github.com/users/globocom',
-		// org_repos  = 'https://api.github.com/orgs/globocom/repos?type=public&per_page=200',
-		org_repos  = 'https://api.github.com/search/repositories?q=user:globocom&sort=stars&order=desc&per_page=3',
-		commits,
-		forks_count,
-		stargazers_count,
 
-		repositories;
+    //renderizar repositorios do globocom
+    let org        = 'https://api.github.com/users/globocom',
+        org_repos  = 'https://api.github.com/search/repositories?q=user:globocom&sort=stars&order=desc&per_page=200',
+        commits,
+        forks_count,
+        stargazers_count,
+        repositories;
 
-	function sortByStars(data) {
-        data.sort(function(a,b) {
-        return b.stargazers_count - a.stargazers_count;
-       });
+    class Repositorio {
+        constructor(nome, count_stars, count_forks){
+            this.nome = nome;
+            this.stars = count_forks;
+            this.forks = count_stars;
+            this.lista_commits = [];
+        }
     }
 
-	$.getJSON(org_repos, function(json){
-		repositories = json.items;
-		// console.log(repositories);
-		// outputPageContent(repositories);
+    class Commit {
+        constructor(author, data, descricao){
+            this.author = author;
+            this.data = data;
+            this.descricao = descricao;
+        }
+    }
 
-		let repositorio = new Repositorios();
-		console.log(repositories);
-		repositorio.lista(repositories);
-	});
+    class Author {
+        constructor(nome, url_photo){
+            this.nome = nome;
+            this.url_photo = url_photo;
+        }
+    }
 
-	class Repositorios {
-	    constructor(nome, count_forks, count_stars){
-	        this.name = name;
-	        this.stargazers_count = stargazers_count;
-	        this.forks_count = forks_count;
-	    }
+    class RenderizadorHtml {
+        renderizarRepositorios(jsonDeRepositorios){
+            for(var i=0; i < jsonDeRepositorios.length; i++){
+                //conteúdo do loop
+                let repositorio = new Repositorio(jsonDeRepositorios[i].name, jsonDeRepositorios[i].stargazers_count, jsonDeRepositorios[i].forks)
+                $('#github-projects').append('<li><a href="'+ repositorio.nome +'" class="repos" data-repo-forks="'+ repositorio.forks +'" data-repo-name="'+ repositorio.nome +'" data-repo-stars="'+ repositorio.stars +'">' + repositorio.nome + '</a></li>');
+            }
+        }
+    }
 
-	    lista(repositories){
-		var lista = $('#github-projects');
+    $.getJSON(org_repos, function(json){
+        let renderer = new RenderizadorHtml();
 
-		for(var i=0; i < repositories.length; i++){
-        	//conteúdo do loop
-        	let repositorio = new Repositorios(repositories[i].name, repositories[i].stargazers_count, repositories[i].forks_count)
-    		console.log(repositorio[i].name);
-    		}
-		}
-	}
+        renderer.renderizarRepositorios(json.items);
+    });
 
-	
-	
 
-	function outputPageContent(data) {
-		// sortByStars(data);
-		
-		$(data).each(function() {
-	        $('#github-projects').append('<li><a href="'+ this.name +'" class="repos" data-repo-forks="'+ this.forks_count +'" data-repo-name="'+ this.name +'" data-repo-stars="'+ this.stargazers_count +'">' + this.name + '</a></li>');
-		});	
-	}
+    function outputPageContent2(data, stars, forks, repo_name) {
+        // $('.stars').empty();
+        $('.stars').html(stars);
 
-	function outputPageContent2(data, stars, forks, repo_name) {
-		// $('.stars').empty();
-		$('.stars').html(stars);
+        // $('.forks').empty();
+        $('.forks').html(forks);
 
-		// $('.forks').empty();
-		$('.forks').html(forks);
+        // $('.repo-name').empty();
+        $('.repo-name').html(repo_name);
 
-		// $('.repo-name').empty();
-		$('.repo-name').html(repo_name);
+        $('#last-commits').empty();
 
-		$('#last-commits').empty();
-
-		$(data).each(function() {
-	        $('#last-commits').append('<li><p>'+ this.commit.author.name +'</p><p>'+ this.commit.message +'</p><p>'+ this.commit.author.date +'</p></li>');
-		});	
-	}
+        $(data).each(function() {
+            $('#last-commits').append('<li><p>'+ this.commit.author.name +'</p><p>'+ this.commit.message +'</p><p>'+ this.commit.author.date +'</p></li>');
+        });
+    }
 
 
 
-	//no clique do repositorio mostrar commits
-	$('#github-projects').on('click', '.repos', function(event){
-		event.preventDefault();
-		var repo_name = $(this).data('repo-name');
-		var repo_stars = $(this).data('repo-stars');
-		var repo_forks = $(this).data('repo-forks');
-		var commits_repo = 'https://api.github.com/repos/globocom/'+repo_name+'/commits?per_page=20'
-			
-		$.getJSON(commits_repo, function(json){
-			commits = json;   
-			console.log(commits);
-			outputPageContent2(commits, repo_stars, repo_forks, repo_name);                
-		});
 
 
-		// console.log(repo_name, commits_repo);
+    //no clique do repositorio mostrar commits
+    $('#github-projects').on('click', '.repos', function(event){
+        event.preventDefault();
+        var repo_name = $(this).data('repo-name');
+        var repo_stars = $(this).data('repo-stars');
+        var repo_forks = $(this).data('repo-forks');
+        var commits_repo = 'https://api.github.com/repos/globocom/'+repo_name+'/commits?per_page=20'
+
+        $.getJSON(commits_repo, function(json){
+            commits = json;
+            console.log(commits);
+            outputPageContent2(commits, repo_stars, repo_forks, repo_name);
+        });
 
 
-	})
+        // console.log(repo_name, commits_repo);
+
+
+    })
 });
