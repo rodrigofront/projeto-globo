669e5a0ba2baf132de8f57b246b896789ec82dea
rodrigofront <fale.rodrigo@gmail.com>
2016-05-20 04:06:30 -0300

    removendo lixo e terminando js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,156 @@
 $(function(){
-    //renderizar repositorios do globocom
-    let org        = 'https://api.github.com/users/globocom',
-        org_repos  = 'https://api.github.com/search/repositories?q=user:globocom&sort=stars&order=desc&per_page=200',
+    let url_org_repos  = 'https://api.github.com/search/repositories?q=user:globocom&sort=stars&order=desc&per_page=100',
         hash       = document.URL.substr(document.URL.indexOf('#')+1),
+        map_repositorios = {
+        	data: []	
+        },
         commits,
+        getPagination,
+        urlPagination,
         commits_repo = '',
-        url_mostra_mais_commits,
         repo_name,
         per_page_commits = 20,
         forks_count,
         stargazers_count,
-        numberPage = 1,
         repositories;
 
+    // CHAMADA PARA EXIBIÇÃO DE TODOS OS REPOSITÓRIOS DA GLOBO.COM NO GITHUB
+	function getMyRepos () {
+		$.ajax({
+		    url: url_org_repos,
+		    type: 'get',
+		    success: function(json, status, xhr){
+	    		map_repositorios.data = map_repositorios.data.concat(json.items);
 
-    function parse_link_header(header) {
-        if (header.length === 0) {
-            throw new Error("input must not be of zero length");
+		    	// verifica se a resposta do request contem o Header "Link"
+		    	if (xhr.getResponseHeader('Link') != null) {
+		    		// verifica se o Header "Link" possui o rel "Next"
+		    		if (parse_link_header(xhr.getResponseHeader('Link')).next != null) {
+    					url_org_repos = parse_link_header(xhr.getResponseHeader('Link')).next;
+	    				getMyRepos();
+	    				return;
+		    		}
+		    	} 
+
+		    	showMyRepos(map_repositorios.data);
+		    	formataJsonRepositorios(map_repositorios.data);
+
+	    	    if(window.location.hash) {
+	    	        repo_name = hash;
+	    	    	commits_repo = 'https://api.github.com/repos/globocom/'+repo_name+'/commits?per_page='+per_page_commits+'';
+	    	    	getPagination = false;
+
+	    			searchRepos(repo_name);
+	    	    };
+		    },
+
+		    error: function(erro){
+				console.log('Erro encontrado:', erro.responseText);
+		    }
+		});	
+	}
+
+	function formataJsonRepositorios(data) {
+		for (var key in data) {
+		  data[key] = {name: data[key].name, stars: data[key].stargazers_count, forks: data[key].forks_count};
+		}
+		map_repositorios = data;
+	}
+
+	function showMyRepos(data) {
+	 	let renderer = new RenderizadorHtml();
+		renderer.renderizarRepositorios(data);
+	}
+
+	function searchRepos(repositorio) {
+		for (var i = 0, len = map_repositorios.length; i < len; i++) {
+		    if (map_repositorios[i].name === repositorio) {
+		        // match is in array[i]
+		        getMyCommits(map_repositorios[i].name, map_repositorios[i].stars, map_repositorios[i].forks);
+		        break;
+		    }
+		}
+	}
+
+	function getMyCommits(repo_name, stars, forks){
+		if (getPagination === false) {
+			commits_repo = commits_repo;	
+		}
+		else {
+			commits_repo = urlPagination;
+		}
+		
+		$.ajax({
+		    url: commits_repo,
+		    type: 'get',
+		    success: function(json, status, xhr){
+		    	// verifica se a resposta do request contem o Header "Link"
+		    	if (xhr.getResponseHeader('Link') != null) {
+		    		// verifica se o Header "Link" possui o rel "Next"
+		    		if (parse_link_header(xhr.getResponseHeader('Link')).next != null) {
+    					urlPagination = parse_link_header(xhr.getResponseHeader('Link')).next;
+						showMyCommits(json, repo_name, stars, forks, urlPagination);
+		    		}
+
+		    		else {
+			    		urlPagination = false;
+				        showMyCommits(json, repo_name, stars, forks, urlPagination);	
+		    		}
+		    	} 
+
+		    },
+		    error: function(erro){
+		      console.log('Erro encontrado:', erro.responseText);
+		    }
+		});
+	}
+
+	function showMyCommits(jsonDeCommits, name, stars, forks, urlPagination) {
+		let renderer = new RenderizadorHtml();
+		renderer.renderizarCommits(jsonDeCommits, name, stars, forks, urlPagination);
+	}
+
+    class RenderizadorHtml {
+        renderizarRepositorios(jsonDeRepositorios){
+            for(var i=0; i < jsonDeRepositorios.length; i++){
+                //conteúdo do loop
+                let repositorio = new Repositorio(jsonDeRepositorios[i].name, jsonDeRepositorios[i].stargazers_count, jsonDeRepositorios[i].forks)
+                $('#github-projects').append('<li><a href="#" class="repos" data-repo-forks="'+ repositorio.forks +'" data-repo-name="'+ repositorio.nome +'" data-repo-stars="'+ repositorio.stars +'">' + repositorio.nome + '</a></li>');
+            }
         }
 
+        renderizarCommits(jsonDeCommits, name, stars, forks, urlPagination){
+	        if (getPagination === false) {
+		        $('.repo-name').html(name);
+
+		        $('.stars').html('Estrela(s):'+stars);
+
+		        $('.forks').html('Fork(s):'+forks);
+	        
+	        	$('#last-commits').empty();	
+	        }
+	        
+
+	        for(var i=0; i < jsonDeCommits.length; i++){
+        	    let commit = new Commit(jsonDeCommits[i].commit.author.name, jsonDeCommits[i].commit.message, jsonDeCommits[i].commit.author.date)
+        	    $('#last-commits').append('<li><p>'+ commit.author +'</p><p>'+ commit.descricao +'</p><p>'+ commit.data +'</p></li>');
+        	}
+
+	        if (urlPagination) {
+	        	$('#box-btn').empty();
+            	$('#box-btn').append('<a href="#" data-url="'+urlPagination+'" class="btn-show-more">Ver mais</a>');	
+	        }
+	        
+	        else {
+	        	$('.btn-show-more').hide();
+	        }	
+        }
+    }
+
+    getMyRepos();
+    
+
+    function parse_link_header(header) {
         // Split parts by comma
         var parts = header.split(',');
         var links = {};
@@ -53,138 +185,23 @@ $(function(){
         }
     }
 
-    // class Author {
-    //     constructor(nome, url_photo){
-    //         this.nome = nome;
-    //         this.url_photo = url_photo;
-    //     }
-    // }
-
-    class RenderizadorHtml {
-        renderizarRepositorios(jsonDeRepositorios){
-            for(var i=0; i < jsonDeRepositorios.length; i++){
-                //conteúdo do loop
-                let repositorio = new Repositorio(jsonDeRepositorios[i].name, jsonDeRepositorios[i].stargazers_count, jsonDeRepositorios[i].forks)
-                $('#github-projects').append('<li><a href="'+ repositorio.nome +'" class="repos" data-repo-forks="'+ repositorio.forks +'" data-repo-name="'+ repositorio.nome +'" data-repo-stars="'+ repositorio.stars +'">' + repositorio.nome + '</a></li>');
-            }
-        }
-
-        renderizarCommits(jsonDeCommits, stars, forks, repo_name, url_mostra_mais_commits){
-        	if (url_mostra_mais_commits) {
-		        $('.stars').html(stars);
-
-		        $('.forks').html(forks);
-
-		        $('.repo-name').html(repo_name);
-
-		        $('#last-commits').empty();
-
-		        for(var i=0; i < jsonDeCommits.length; i++){
-	        	    let commit = new Commit(jsonDeCommits[i].commit.author.name, jsonDeCommits[i].commit.message, jsonDeCommits[i].commit.author.date)
-	        	    $('#last-commits').append('<li><p>'+ commit.author +'</p><p>'+ commit.descricao +'</p><p>'+ commit.data +'</p></li>');
-	        	}
-		        
-	        	$('#box-btn').empty();
-            	$('#box-btn').append('<a href="#" class="btn-show-more">Ver mais</a>');
-		        
-
-		        else {
-	        		$('.btn-show-more').hide();
-		        }
-        	}
-
-	        else {
-	        	for(var i=0; i < jsonDeCommits.length; i++){
-    		        let commit = new Commit(jsonDeCommits[i].commit.author.name, jsonDeCommits[i].commit.message, jsonDeCommits[i].commit.author.date)
-    		        $('#last-commits').append('<li><p>'+ commit.author +'</p><p>'+ commit.descricao +'</p><p>'+ commit.data +'</p></li>');
-    		    }
-
-	        	if (jsonDeCommits.length != url_mostra_mais_commits) {
-    				$('.btn-show-more').hide();
-	        	}
-	        }
-        }
-    }
-
-    $.ajax({
-        url: org_repos,
-        type: 'get',
-        success: function(json){
-            let renderer = new RenderizadorHtml();
-        	renderer.renderizarRepositorios(json.items);
-
-        	if(window.location.hash) {
-        	    repo_name = hash;
-        	    commits_repo = 'https://api.github.com/repos/globocom/'+hash+'/commits?per_page='+per_page_commits+'&page='+numberPage+'';
-
-        	    $.ajax({
-        	        url: commits_repo,
-        	        type: 'get',
-        	        success: function(json, status, xhr){
-        	            let renderer = new RenderizadorHtml();
-        	            renderer.renderizarCommits(json, 5, 2, hash);
-        	            // console.log(xhr.getResponseHeader('Link'));
-
-        	            
-        	        },
-        	        error: function(erro){
-        	          console.log('deu erro', erro);
-        	        }
-        	    });
-        	};
-        },
-
-        error: function(erro){
-          console.log('deu erro', erro);
-        }
-    });
-
     //no clique do repositorio mostrar commits
     $('#github-projects').on('click', '.repos', function(event){
-    	numberPage = 1;
         event.preventDefault();
-        	repo_name = $(this).data('repo-name');
-        var repo_stars = $(this).data('repo-stars');
-        var repo_forks = $(this).data('repo-forks');
-        	commits_repo = 'https://api.github.com/repos/globocom/'+repo_name+'/commits?per_page='+per_page_commits+'&page='+numberPage+'';
+    	repo_name = $(this).data('repo-name');
+    	commits_repo = 'https://api.github.com/repos/globocom/'+repo_name+'/commits?per_page='+per_page_commits+'';
+    	getPagination = false;
 
 		window.location.hash = repo_name;
 
-		$.ajax({
-		    url: commits_repo,
-		    type: 'get',
-		    success: function(json, status, xhr){
-		        let renderer = new RenderizadorHtml();
-            	
-            	if (xhr.getResponseHeader('Link') == null) {
-            		renderer.renderizarCommits(json, repo_stars, repo_forks, repo_name, xhr.getResponseHeader('Link'));	
-            	}
-
-            	else {
-        			renderer.renderizarCommits(json, repo_stars, repo_forks, repo_name, parse_link_header(xhr.getResponseHeader('Link')).next);
-            	}
-		    },
-		    error: function(erro){
-		      console.log('deu erro', erro);
-		    }
-		});
+		searchRepos(repo_name);
     });
 
     $('#box-btn').on('click', '.btn-show-more', function(event){
-    	numberPage++;
-    	commits_repo = url_mostra_mais_commits;
     	event.preventDefault();
+    	getPagination = true;
 
-		$.ajax({
-		    url: commits_repo,
-		    type: 'get',
-		    success: function(json){
-		        let renderer = new RenderizadorHtml();
-        		renderer.renderizarCommits(json, null, null, null);
-		    },
-		    error: function(erro){
-		      console.log('deu erro', erro);
-		    }
-		});
+    	searchRepos(repo_name);
+		
 	});
 });
